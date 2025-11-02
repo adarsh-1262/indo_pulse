8828e828e22312dcdfaff954629233ddfe2cf2ac
ADARSH SINGH <9792051262a@gmail.com>
2025-11-02 15:07:50 +0530

    installed shadcn

--- indo_pulse_frontend/src/App.jsx
+++ indo_pulse_frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <Button className='bg-red-400' variant='ghost'>Click Me</Button>
+    </div>
+    
+    
   )
 }
 
