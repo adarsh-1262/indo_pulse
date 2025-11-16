6794eae81659941798c41db64030f64f448d8e16
ADARSH SINGH <9792051262a@gmail.com>
2025-11-16 21:28:16 +0530

    Completed signup Functionality

--- indo_pulse_frontend/src/auth/forms/SignUpForm.jsx
+++ indo_pulse_frontend/src/auth/forms/SignUpForm.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -13,14 +13,26 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { toast } from "sonner"
 
 const formSchema = z.object({
-  username: z.string().min(2, { message: "Username must be at least 2 characters long" }).max(50, { message: "Username must be at most 50 characters long" }),
-  email: z.string().email({ message: "Invalid email address" }),
-  password: z.string().min(8, { message: "Password must be at least 8 characters long" }).max(100, { message: "Password must be at most 100 characters long" }),
-})
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters long" })
+    .max(50, { message: "Username must be at most 50 characters long" }),
+  email: z.email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(100, { message: "Password must be at most 100 characters long" }),
+});
 
 const SignUpForm = () => {
+  const navigate = useNavigate()
+
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+
   // 1. Define your form
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -29,53 +41,82 @@ const SignUpForm = () => {
       email: "",
       password: "",
     },
-  })
+  });
 
   // 2. Define a submit Handler
-  function onSubmit(values) {
+  async function onSubmit(values) {
+    try {
+      setLoading(true)
+      setErrorMessage(null)
 
-    console.log(values)
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      })
+
+      const data = await res.json()
+
+      if (data.success === false) {
+        setLoading(false)
+        toast.error("Sign up failed! Please try again.")
+
+        return setErrorMessage(data.message)
+      }
+
+      setLoading(false)
+
+      if (res.ok) {
+        toast.success("Sign up Successful!")
+        navigate("/signin")
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+      toast.error("Something went wrong!")
+    }
   }
   return (
-    <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl sm:max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-      {/* Left Side - Image */}
-      <div className='flex-1'>
-        <Link
-         to={"/"}
-         className='font-bold text-2xl sm:text-4xl flex flex-wrap'
-        >
-          <span className='text-red-700'>Indo</span>
-          <span className='text-red-900'>Pulse</span>
-        </Link>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl sm:max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* Left Side - Image */}
+        <div className="flex-1">
+          <Link
+            to={"/"}
+            className="font-bold text-2xl sm:text-4xl flex flex-wrap"
+          >
+            <span className="text-red-700">Indo</span>
+            <span className="text-red-900">Pulse</span>
+          </Link>
 
-        <h2 className='text-[24px] md:text-[30px] font-bold leading-[140%] tracking-tighter pt-5 sm:pt-12'>
-          Create a new account
-        </h2>
+          <h2 className="text-[24px] md:text-[30px] font-bold leading-[140%] tracking-tighter pt-5 sm:pt-12">
+            Create a new account
+          </h2>
 
-        <p className='text-slate-500 text-[14px] font-medium leading-[140%] md:text-[16px] md:font-normal mt-2'>
-          Welcome to IndoPulse! Please fill in the details below to create your account.
-        </p>
-      </div>
+          <p className="text-slate-500 text-[14px] font-medium leading-[140%] md:text-[16px] md:font-normal mt-2">
+            Welcome to IndoPulse! Please fill in the details below to create
+            your account.
+          </p>
+        </div>
 
-      {/* Right Side - Form */}
-      <div className='flex-1'>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input type='text' placeholder="username" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
+        {/* Right Side - Form */}
+        <div className="flex-1">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
                 control={form.control}
                 name="email"
                 render={({ field }) => (
@@ -94,32 +135,48 @@ const SignUpForm = () => {
                   </FormItem>
                 )}
               />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type='password' placeholder="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className='w-full bg-red-950'>Submit</Button>
-          </form>
-        </Form>
-        <div className='flex gap-2 text-sm mt-5'>
-        <span>Already have an account?</span>
-        <Link to={"/signin"} className='text-red-700 font-medium'>
-          Sign In
-        </Link>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button 
+                type="submit" 
+                className="w-full bg-red-950"
+                disabled={loading}
+              >
+                {loading ? (
+                  <span className="animate-pulse">Loading...</span>
+                ) : (
+                  <span>Sign Up</span>
+                )}
+              </Button>
+            </form>
+          </Form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Already have an account?</span>
+            <Link to={"/signin"} className="text-red-700 font-medium">
+              Sign In
+            </Link>
+          </div>
+
+          {errorMessage && <p className="mt-5 text-red-500">{errorMessage}</p>}
         </div>
       </div>
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
